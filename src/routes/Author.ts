11e6388c9ceab0ce7eadab678685ac694bd0c60f
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2023-05-23 13:38:43 +0300

    added Joi Validations to the routes

--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controllers from '../controllers/Author';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', controllers.createAuthor);
+router.post('/create', ValidateSchema(Schemas.author.create), controllers.createAuthor);
 router.get('/get/:authorId', controllers.readAuthor);
 router.get('/get', controllers.readAll);
-router.patch('/update/:authorId', controllers.UpdateAuthor);
+router.patch('/update/:authorId', ValidateSchema(Schemas.author.update), controllers.UpdateAuthor);
 router.delete('/delete/:authorId', controllers.DeleteAuthor);
 
 export default router;
--- src/routes/Book.ts
+++ src/routes/Book.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controllers from '../controllers/Book';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', controllers.createBook);
+router.post('/create', ValidateSchema(Schemas.book.create), controllers.createBook);
 router.get('/get/:bookId', controllers.readBook);
 router.get('/get', controllers.readAll);
-router.patch('/update/:bookId', controllers.UpdateBook);
+router.patch('/update/:bookId', ValidateSchema(Schemas.book.create), controllers.UpdateBook);
 router.delete('/delete/:bookId', controllers.DeleteBook);
 
 export default router;
